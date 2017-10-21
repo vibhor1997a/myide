d53bd407fdf32c7ae3131e34d3c29d8afd6e15ac
vibhor1997a <vibhor349a@rediffmail.com>
2017-10-21 22:53:35 +0530

    Working

--- public/javascripts/abc.js
+++ public/javascripts/abc.js
@@ -1,12 +1,48 @@
 $(document).ready(function () {
     $('#run').click(function () {
-        alert('Hello!');
         $.post('/run', {
-            code: $('#code').val()
-        ,   stdin: $('#stdin').val() 
+            code: editor.getValue()
+            , stdin: $('#stdin').val()
+            , lang: $('#lang-selector').val()
         }, function (data, status) {
-            console.log("Done!", status);
             console.log(data);
+            if (data.stdout) {
+                $('#stdout').val(data.stdout);
+                $('#stdout').show();
+            }
+            else{
+                $('#stdout').hide();                
+            }
+            if (data.stderr) {
+                $('#stderr').val(data.stderr);
+                $('#stderr').show();
+            }
+            else{
+                $('#stderr').hide();                
+            }
         });
+    });
+    $('#lang-selector').change(function () {
+        console.log('changed!');
+        if ($('#lang-selector').val() == 'C' || $('#lang-selector').val() == 'C++') {
+            editor.getSession().setMode("ace/mode/c_cpp");
+        }
+        else if ($('#lang-selector').val() == 'Java') {
+            editor.getSession().setMode("ace/mode/java");
+        }
+        else if ($('#lang-selector').val() == 'Python') {
+            editor.getSession().setMode("ace/mode/python");
+        }
+        else {
+            editor.getSession().setMode("ace/mode/javascript");
+        }
+    });
+    $('#toggle-theme').click(function () {
+        if ($('#toggle-theme').prop('checked')) {
+            editor.setTheme("ace/theme/twilight");
+        }
+        else{
+            editor.setTheme("");            
+        }
     });
 });
--- routes/run.js
+++ routes/run.js
@@ -1,91 +1,52 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-var spawn = require("child_process").spawn;
 var fs = require('fs');
+var compile_run = require('compile-run');
 
-
-/*Async function to run c code and pass stdout,stderr to the callback fn*/
-function runC(fileName, stdin, callback) {
-    var proc = spawn('gcc', ["./code/c/"+fileName+".c", "-o", "./code/c/"+fileName]);
-    var stdout = "";
-    var stderr = "";
-    var f = false;
-    var timeout = setTimeout(function () {
-        proc.stdin.pause();
-        proc.kill();
-        f = true;
-    }, 2000);
-    proc.stderr.on('data', function (_stderr) {
-        stderr += _stderr;
-    });
-    proc.stderr.on('end', function () {
-        proc.kill();
-        f = true;
-    });
-    proc.on('close', function (code) {
-        proc.kill();
-        f=true;
-        if (code == 0) {
-                console.log(code);
-            var proc1 = spawn('./code/c/'+fileName+'.exe');
-            var stdout1 = "";
-            var stderr1 = "";
-            var fl = false;
-            var timeout1 = setTimeout(function () {
-                proc1.stdin.pause();
-                proc1.kill();
-                fl = true;
-            }, 2000);
-            if(stdin){
-            proc1.stdin.write(stdin+"\n");
-            }
-            proc1.stdout.on('data', function (_stdout) {
-                stdout1 += _stdout;
-            });
-            proc1.stdout.on('end', function () {
-                proc.kill();
-                fl = true;
-            });
-            proc1.stderr.on('data', function (_stderr) {
-                stderr1 += _stderr;
-            });
-            proc1.stderr.on('end', function () {
-                proc.kill();
-                fl = true;
-            });
-            proc1.on('close',function(code){
-                if(code==0)
-                callback(stdout1,"");
-                else{    
-                callback("","NZEC");    
-                }
-            });
-            if (fl) {
-                clearTimeout(timeout1);
-            }
-        }
-        else{
-        callback("",stderr);            
-        }
-    });
-    if (f) {
-        clearTimeout(timeout);
+function run(lang, code, input, callback) {
+    if (lang == 'C') {
+        compile_run.runC(code, input, function (stdout, stderr, err) {
+            callback(stdout, stderr, err);
+        });
+    }
+    else if (lang == 'C++') {
+        compile_run.runCpp(code, input, function (stdout, stderr, err) {
+            callback(stdout, stderr, err);
+        });
+    }
+    else if (lang == 'Java') {
+        compile_run.runJava(code, input, function (stdout, stderr, err) {
+            callback(stdout, stderr, err);
+        });
+    }
+    else if (lang == 'Python') {
+        compile_run.runPython(code, input, function (stdout, stderr, err) {
+            callback(stdout, stderr, err);
+        });
+    }
+    else if (lang == 'Node.js') {
+        compile_run.runNode(code, input, function (stdout, stderr, err) {
+            callback(stdout, stderr, err);
+        });
+    }
+    else {
+        callback("", "", "Invalid Language!");
     }
 }
-router.get('/', function (req, res, next) {
 
-});
 router.post('/', function (req, res, next) {
     var code = req.body.code.trim();
-    var fileName = 'abc-'+Math.floor(Math.random()*100000);
-    var input=req.body.stdin.trim();
-    fs.writeFile('./code/c/' + fileName+'.c', code, function (err) {
+    var input = req.body.stdin.trim();
+    var lang = req.body.lang;
+
+    run(lang, code, input, function (stdout, stderr, err) {
         if (!err) {
-            runC(fileName,input,function(stdout,stderr){
-                console.log(stdout);
-                console.log(stderr);
-                    res.send({stdout:stdout,stderr:stderr});
+            console.log(stdout);
+            console.log(stderr);
+            res.send({
+                stdout: stdout
+                , stderr: stderr
             });
         }
         else {
